456f6146c2462649791e553e1985d45ba36a2a22
Jhonatta <jhonattasantos27@gmaol.com>
2025-02-24 06:34:34 -0300

    feat: add lombook to the class for abstraction

--- src/main/java/com/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/com/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -2,13 +2,18 @@ package com.java10x.CadastroDeNinjas.Missoes;
 
 import com.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name="tb_missoes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MissoesModel {
-
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,31 +24,5 @@ public class MissoesModel {
     //@OneToMany Uma missao pode ter varios ninjas
     @OneToMany(mappedBy = "missoes")
     private List<NinjaModel> ninjas;
-
-    public MissoesModel() {
-    }
-
-    public MissoesModel(Long id, String nome, String dificuldade, NinjaModel ninja) {
-        this.id = id;
-        this.nome = nome;
-        this.dificuldade = dificuldade;
-        this.ninja = ninja;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getDificuldade() {
-        return dificuldade;
-    }
-
-    public void setDificuldade(String dificuldade) {
-        this.dificuldade = dificuldade;
-    }
 
 }
