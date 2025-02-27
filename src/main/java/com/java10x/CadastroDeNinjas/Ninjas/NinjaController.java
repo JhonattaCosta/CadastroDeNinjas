4a7467cce13d770540bb35d36c31ac041bc0b4d5
Jhonatta <jhonattasantos27@gmaol.com>
2025-02-27 06:48:31 -0300

    Feat: Updates controller with data mock

--- src/main/java/com/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/com/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,8 +1,6 @@
 package com.java10x.CadastroDeNinjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -11,6 +9,33 @@ public class NinjaController {
     @GetMapping("/boasvindas")
     public String boasVindas(){
         return "Essa é minha primeira mensagem nessa rota";
+    }
+
+    //Adicionar Ninja
+    @PostMapping("/adicionar ")
+    public String criarNinja(){
+        return "Ninja Criado";
+    }
+    //Procurar Ninjas
+    @GetMapping("/todos")
+    public String mostrarTodosOsNinjas(){
+        return "Ninjas";
+    }
+    //Procurar Ninjas por ID
+    @GetMapping("/todosID")
+    public String mostrarTodosOsNinjasID(){
+        return "Ninjas ID";
+    }
+    //Alterar Dados do NInjas
+    @PutMapping("/alterarID")
+    public String alterarNinjaPorID(){
+        return "Alterar Ninja por ID";
+    }
+
+    //Deletar Ninja
+    @DeleteMapping("/DeletarID")
+    public String deletaNinjaPorID(){
+        return "Deletar ninja por ID";
     }
 
 }
